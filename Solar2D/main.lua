9a078e559adb2cc522bc18278b5dc42cb7abc3cc
Steeve-feuille-ciseaux <libertybanshee1@gmail.com>
2025-08-22 14:48:20 +0200

    v0.00.2

--- Solar2D/main.lua
+++ Solar2D/main.lua
@@ -4,4 +4,20 @@
 --
 -----------------------------------------------------------------------------------------
 
--- Your code here
+-- Désactiver le lissage (important pour le pixel art)
+display.setDefault("magTextureFilter", "nearest")
+display.setDefault("minTextureFilter", "nearest")
+
+-- Définir la couleur de fond (optionnel)
+-- display.setDefault("background", 0.1, 0.1, 0.1)  -- Gris foncé
+
+-- Charger et afficher l'image
+local monImage = display.newImage("sprite/swordsman/Idle1_Swordsman.png")
+
+-- Centrer l'image à l'écran
+monImage.x = display.contentCenterX
+monImage.y = display.contentCenterY
+
+-- Doubler la taille (x2)
+monImage.xScale = 8
+monImage.yScale = 8
